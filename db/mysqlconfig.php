cd233f5313cf05bc279e8ec46ea49bec4ee1831f
xxn <xxn19960511@163.com>
2018-05-01 11:50:08 +0800

    插入数据

--- db/mysqlconfig.php
+++ db/mysqlconfig.php
@@ -3,5 +3,5 @@ define('DB_HOST', 'localhost:3306');
 define('DB_USER', 'root');
 define('DB_PWD', 'toor1234');
 define('DB_CHARSET', 'UTF8');
-define('DB_DBNAME', 'test');
+define('DB_DBNAME', 'saibaite');
 ?>
--- test.php
+++ test.php
@@ -1,14 +1,20 @@
 <?php
 require_once 'db/mysqlfunction.php';
 require_once 'db/mysqlconfig.php';
-
+ini_set('date.timezone','Asia/Shanghai');
 $db = new mysql();
 $link = $db->connect2();
 
-
-$sql='SELECT * FROM one';
-$rows = $db->fetchAll($sql);
-var_dump($rows);
-
+//数据库操作实例插入数据
+//$now = date("Y-m-d H:i:s");
+//echo $now;
+//die();
+$arr = array(
+    "tem" => 1.25,
+    "humidity" => 132,
+    "creat_time" => date("Y-m-d H:i:s")
+);
+var_dump($arr);
+$db->insert($arr,t_h);
 
 ?>
