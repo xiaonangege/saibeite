5c137c848e2127896f7f92743ce3dbf1dd3ba885
小南咯咯 <xxn19960511@163.com>
2018-05-28 22:08:12 +0800

    Update mysqlconfig.php

--- db/mysqlconfig.php
+++ db/mysqlconfig.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost:3306');
 define('DB_USER', 'root');
-define('DB_PWD', 'toor1234');
+define('DB_PWD', '');
 define('DB_CHARSET', 'UTF8');
 define('DB_DBNAME', 'test');
 ?>
