9ffb087e2fa0f507866d84baa2ce0eb58564e036
小南咯咯 <xxn19960511@163.com>
2018-05-28 22:42:35 +0800

    Update led_con.php

--- test/led_con.php
+++ test/led_con.php
@@ -5,8 +5,8 @@
  * Date: 2018/5/28
  * Time: 17:37
  */
-require_once 'db/mysqlfunction.php';
-require_once 'db/mysqlconfig.php';
+require_once '../db/mysqlfunction.php';
+require_once '../db/mysqlconfig.php';
 require_once 'function.php';//调用函数
 if(!$_POST){
     ajaxCallback(-1,"未获取post传值");
