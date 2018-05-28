6c8861a5f92dbfdce2faf0f827f76cb45aef5e2d
小南咯咯 <xxn19960511@163.com>
2018-05-28 23:11:22 +0800

    Update led_con.php

--- test/led_con.php
+++ test/led_con.php
@@ -8,19 +8,19 @@
 require_once '../db/mysqlfunction.php';
 require_once '../db/mysqlconfig.php';
 require_once '../function.php';//调用函数
-//if(!$_POST){
-  //  ajaxCallback(-1,"未获取post传值");
-//}
-//$id=$_POST['id'];
-//$state=$_POST['state'];
-$id=1;
-$state=1;
+$db = new mysql();
+$link = $db->connect2();
+if(!$_POST){
+    ajaxCallback(-1,"未获取post传值");
+}
+$id=$_POST['id'];
+$state=$_POST['state'];
+
 $arr=array(
 	'state'=>$state
 );
 $con='id='.$id;
 $table='switchon';
-echo $con;
 if ($db->update($arr,$table,$con)) {//插入数据操作
 	ajaxCallback(0,"添加成功");
 }
