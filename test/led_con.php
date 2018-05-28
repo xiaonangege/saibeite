79dcb65fd8b9835a4d310cd8e03b503afc80db9e
小南咯咯 <xxn19960511@163.com>
2018-05-28 22:53:50 +0800

    Update led_con.php

--- test/led_con.php
+++ test/led_con.php
@@ -8,12 +8,11 @@
 require_once '../db/mysqlfunction.php';
 require_once '../db/mysqlconfig.php';
 require_once '../function.php';//调用函数
-//if(!$_POST){
-//    ajaxCallback(-1,"未获取post传值");
-//}
+if(!$_POST){
+    ajaxCallback(-1,"未获取post传值");
+}
 $id=$_POST['id'];
 $state=$_POST['state'];
-ajaxCallback(0,"添加成功",$id);
 $arr=array(
 	'state'=>$state
 );
