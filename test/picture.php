4db72862e82e08052fab240afc105d2db6ca9282
xiaoxiaonan <xiaoxiaonan@sc-edu.com>
2018-05-30 21:46:12 +0800

    picture

---
+++ test/picture.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 13248
+ * Date: 2018/5/27
+ * Time: 18:08
+ */
+require_once '../db/mysqlfunction.php';
+require_once '../db/mysqlconfig.php';
+require_once '../function.php';//调用函数
+ini_set('date.timezone','Asia/Shanghai');//设置时间时区
+$db = new mysql();
+$link = $db->connect2();
+
+$sql='select * from `picture` order by `id` ASC';
+
+$result=$db->fetchAll($sql);
+ajaxCallback(0,"成功",$result);
+
